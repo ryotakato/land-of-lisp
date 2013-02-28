9aeca2fb7b5e1dcf7aed2bc36a29942016f660ce
ryotakato <ryotakato4@gmail.com>
2013-03-01 02:05:38 +0900

    finito 02 guess

--- 02_guess.scm
+++ 02_guess.scm
@@ -1,17 +1,34 @@
+(use srfi-13)
 
 (define *small* 1)
 (define *big* 100)
 
-(print *big*)
-
-(define *big* 10)
-
-(print *big*)
+(print (string-concatenate (cons "small:" (cons (number->string *small*) '()))))
+(print (string-concatenate (cons "big:" (cons (number->string *big*) '()))))
 
 
-(define guess-my-number
-  (cons '1 '()
+
+(define (guess-my-number)
+  (ash (+ *big* *small*) -1
    )
   )
 
-(print (guess-my-number))
+(define (smaller)
+  (set! *big* (- (guess-my-number) 1))
+  (guess-my-number)
+  )
+
+(define (bigger)
+  (set! *small* (+ (guess-my-number) 1))
+  (guess-my-number)
+  )
+
+(define (start-over)
+  (set! *small* 1)
+  (set! *big* 100)
+  (guess-my-number)
+  )
+
+(print (bigger))
+(print (smaller))
+(print (smaller))
